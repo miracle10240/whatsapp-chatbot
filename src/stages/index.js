de142205382225aebcb09c3a928267f1e70a3189
miracle10240 <147319725+miracle10240@users.noreply.github.com>
2024-03-13 09:53:58 +0100

    Update index.js

--- src/stages/index.js
+++ src/stages/index.js
@@ -4,5 +4,27 @@ import { stageTwo } from './2.js';
 import { stageThree } from './3.js';
 import { stageFour } from './4.js';
 import { finalStage } from './5.js';
+async function start(client) {
+  await client.onMessage(async (message) => {
+    try {
+      const currentStage = getStage({ from: message.from });
 
-export { initialStage, stageOne, stageTwo, stageThree, stageFour, finalStage };
+      const messageResponse = stages[currentStage].stage.exec({
+        from: message.from,
+        message: message.body,
+        client,
+      });
+
+      if (messageResponse) {
+        await client.sendText(message.from, messageResponse);
+      }
+    } catch (error) {
+      client.close();
+    }
+  });
+
+  process.on('SIGINT', function () {
+    client.close();
+  });
+}
+export { initialStage, stageOne, stageTwo, stageThree, stageFour, finalStage, start };
